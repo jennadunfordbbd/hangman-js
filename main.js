44f9005fbb3a5435e70ad617002ae400052ffb95
Jenna_Dunford <Jenna.Dunford@bbd.co.za>
2023-02-21 08:23:12 +0200

    making the little man

--- main.js
+++ main.js
@@ -2,6 +2,15 @@
 const wordPromise = fetch('words.txt');
 let currentWord = "";
 let currentElement;
+let correctLetters = 0;
+let incorrectTries = 0;
+let maxTries = 7;
+
+const alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+
+let triesElement = document.getElementById("triesCounter");
+triesElement.innerHTML = "Tries" + incorrectTries.toString() + "/" + maxTries.toString();
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -15,37 +24,63 @@ async function fetchData() {
 
 
 async function chooseRandomWord(){
+  resetAll();
     let wordsArray = [];
 
   wordsArray = await fetchData().then( arr => arr);
   let randomNum = getRandomInt(wordsArray.length);
-  console.log(randomNum);
 
   currentWord= wordsArray[randomNum];
   currentWord = currentWord.toUpperCase();
   document.getElementById("alphabet").classList.remove("hide");
-  document.getElementById("displayWord").innerHTML = currentWord;
 
 for(const element of currentWord){
         let node = document.createElement("LI");
         let textnode = document.createTextNode(element);
         node.appendChild(textnode);
+        node.setAttribute("id","wordList");
         document.getElementById("letterList").appendChild(node);
 }
 
 }
 
-function checkIfLetterInWord(letter,event){
-  console.log(letter);
-
-console.log(event.target);
-console.log(currentWord.indexOf(letter));
-if(currentWord.indexOf(letter) == -1){
-    console.log("does not exist in word");
+function checkIfLetterInWord(event){
+if(currentWord.indexOf(event.target.innerHTML) == -1){
     hideWord(event.target,false);
+    incorrectTries = incorrectTries + 1;
+    let triesElement = document.getElementById("triesCounter");
+    triesElement.innerHTML = "Tries" + incorrectTries.toString() + "/" + maxTries.toString();
 }else{
-  console.log("Does exist in word");
+
   hideWord(event.target,true);
+  let element = document.getElementById("letterList");
+  for(let child=element.firstChild; child!==null; child=child.nextSibling) {
+    if(child.innerHTML == event.target.innerHTML){
+      child.classList.add("correctLetters");
+      correctLetters = correctLetters + 1;
+    }
+    
+}
+}
+if(incorrectTries == maxTries){
+  alert("You Lose, the word was: " + currentWord);
+  let playButton = document.getElementById("playAgainButton");
+  let alphaElem = document.getElementById("alphabetL");
+  for(let child=alphaElem.firstChild; child!==null; child=child.nextSibling) {
+    child.onclick = null;
+}
+  
+
+  playButton.classList.remove("hide");
+}
+if(correctLetters==currentWord.length){
+  alert("You win! The word was: " + currentWord);
+  let playButton = document.getElementById("playAgainButton");
+  let alphaElem = document.getElementById("alphabetL");
+  for(let child=alphaElem.firstChild; child!==null; child=child.nextSibling) {
+    child.onclick = null;
+}
+  playButton.classList.remove("hide");
 }
 }
 
@@ -58,4 +93,34 @@ function hideWord(element,check){
     element.onclick = null;
   }
 
+}
+
+function resetAll(){
+  incorrectTries = 0;
+  correctLetters = 0;
+  currentWord="";
+  let triesElement = document.getElementById("triesCounter");
+  triesElement.innerHTML = "Tries" + incorrectTries.toString() + "/" + maxTries.toString();
+  let playButton = document.getElementById("playAgainButton");
+  playButton.classList.add("hide");
+  let element = document.getElementById("letterList");
+  while (element.firstChild) {
+    element.removeChild(element.firstChild);
+  }
+  let alphaElem = document.getElementById("alphabetL");
+  while(alphaElem.firstChild){
+    alphaElem.removeChild(alphaElem.firstChild);
+  }
+  makeAlphabet();
+  
+}
+
+function makeAlphabet(){
+  for(const letter of alphabet){
+    let node = document.createElement("LI");
+    let textnode = document.createTextNode(letter);
+    node.appendChild(textnode);
+    node.setAttribute("onclick","checkIfLetterInWord(event);")
+    document.getElementById("alphabetL").appendChild(node);
+  }
 }
